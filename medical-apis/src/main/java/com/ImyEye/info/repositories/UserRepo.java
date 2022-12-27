28a9388964708b7766366a67f6b1aee1ad646bd0
pilotsatyam <satyamsrivastaviit@gmail.com>
2022-12-27 12:37:08 +0530

    Deleted

--- medical-apis/src/main/java/com/ImyEye/info/repositories/UserRepo.java
+++
@@ -1,9 +0,0 @@
-package com.ImyEye.info.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ImyEye.info.entities.User;
-
-public interface UserRepo extends JpaRepository<User,Integer>{
-
-}
